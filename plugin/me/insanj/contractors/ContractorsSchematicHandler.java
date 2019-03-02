a2b8594c70957affdc2ae9012d469d56836744e5
insanj <me@insanj.com>
2019-03-02 01:04:54 -0500

    :tada: successfully read from .schematic using built in NBT parsing

--- plugin/me/insanj/contractors/ContractorsSchematicHandler.java
+++ plugin/me/insanj/contractors/ContractorsSchematicHandler.java
@@ -11,10 +11,6 @@ import java.util.ArrayList;
 
 import net.minecraft.server.v1_13_R2.*;
 
-import eisenwave.nbt.*;
-import eisenwave.nbt.io.*;
-
-// derived from https://bukkit.org/threads/how-the-heck-do-i-read-a-schematic-file.45065/
 public class ContractorsSchematicHandler {
     public final Contractors plugin;
 
@@ -32,28 +28,39 @@ public class ContractorsSchematicHandler {
                 System.out.println("Reading schematic file at path: " + filename);
             }
 
-            /*InputStream fis = this.plugin.getClass().getResourceAsStream(filename);
+            InputStream fis = new FileInputStream(f);
             NBTTagCompound nbtdata = NBTCompressedStreamTools.a(fis);
  
             short width = nbtdata.getShort("Width");
             short height = nbtdata.getShort("Height");
             short length = nbtdata.getShort("Length");
  
+            String materials = nbtdata.getString("Materials");
+
             byte[] blocks = nbtdata.getByteArray("Blocks");
             byte[] data = nbtdata.getByteArray("Data");
  
             NBTTagList entities = nbtdata.getList("Entities", 0);
             NBTTagList tileentities = nbtdata.getList("TileEntities", 0);
 
-            fis.close();*/
+            fis.close();
 
+            /*
             NBTNamedTag namedTag = new NBTDeserializer().fromFile(f);
             // the NBT file format dictates that the root tag MUST be a compound
             NBTCompound root = (NBTCompound) namedTag.getTag();
             NBTCompound data = root.getCompoundTag("Data");
-            
+            */
+
             HashMap<String, Object> response = new HashMap<String, Object>();
-            response.put("data", data.toString());
+            response.put("Width", width);
+            response.put("Height", height);
+            response.put("Length", length);
+            response.put("Materials", materials);
+            response.put("Blocks", blocks.toString());
+            response.put("Data", data.toString());
+            response.put("Entities", entities.toString());
+            response.put("TileEntities", tileentities.toString());
             return response;
         } catch (Exception e) {
             e.printStackTrace();
