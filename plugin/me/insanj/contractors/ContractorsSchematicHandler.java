44d141f492f7285780038a19e7c40bb40275d6ce
insanj <me@insanj.com>
2019-03-02 00:52:41 -0500

    numerous attempts with einsenwave nbt proj

--- plugin/me/insanj/contractors/ContractorsSchematicHandler.java
+++ plugin/me/insanj/contractors/ContractorsSchematicHandler.java
@@ -11,6 +11,9 @@ import java.util.ArrayList;
 
 import net.minecraft.server.v1_13_R2.*;
 
+import eisenwave.nbt.*;
+import eisenwave.nbt.io.*;
+
 // derived from https://bukkit.org/threads/how-the-heck-do-i-read-a-schematic-file.45065/
 public class ContractorsSchematicHandler {
     public final Contractors plugin;
@@ -29,7 +32,7 @@ public class ContractorsSchematicHandler {
                 System.out.println("Reading schematic file at path: " + filename);
             }
 
-            InputStream fis = this.plugin.getClass().getResourceAsStream(filename);
+            /*InputStream fis = this.plugin.getClass().getResourceAsStream(filename);
             NBTTagCompound nbtdata = NBTCompressedStreamTools.a(fis);
  
             short width = nbtdata.getShort("Width");
@@ -42,16 +45,15 @@ public class ContractorsSchematicHandler {
             NBTTagList entities = nbtdata.getList("Entities", 0);
             NBTTagList tileentities = nbtdata.getList("TileEntities", 0);
 
-            fis.close();
+            fis.close();*/
+
+            NBTNamedTag namedTag = new NBTDeserializer().fromFile(f);
+            // the NBT file format dictates that the root tag MUST be a compound
+            NBTCompound root = (NBTCompound) namedTag.getTag();
+            NBTCompound data = root.getCompoundTag("Data");
             
             HashMap<String, Object> response = new HashMap<String, Object>();
-            response.put("width", width);
-            response.put("height", height);
-            response.put("length", length);
-            response.put("blocks", blocks);
-            response.put("data", data);
-            response.put("entities", entities);
-            response.put("tileentities", tileentities);
+            response.put("data", data.toString());
             return response;
         } catch (Exception e) {
             e.printStackTrace();
